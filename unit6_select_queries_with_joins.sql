1457a0437e0735035c28a3a016073e89ef5d48d1
Abdiel Ortega <38680546+jabdi13@users.noreply.github.com>
2024-02-03 23:15:03 -0600

    Create unit6_select_queries_with_joins.sql

---
+++ unit6_select_queries_with_joins.sql
@@ -0,0 +1,15 @@
+SELECT * FROM movies
+INNER JOIN boxoffice
+    ON movies.id = boxoffice.movie_id;
+
+SELECT title, domestic_sales, international_sales
+FROM movies
+  JOIN boxoffice
+    ON movies.id = boxoffice.movie_id
+WHERE international_sales > domestic_sales;
+
+SELECT title, rating
+FROM movies
+  INNER JOIN boxoffice
+    ON movies.id = boxoffice.movie_id
+ORDER BY rating DESC;
