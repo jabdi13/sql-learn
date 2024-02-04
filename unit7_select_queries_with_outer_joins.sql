777ba4a7c08fdc7cdc353e79be31385b347cdf15
Abdiel Ortega <38680546+jabdi13@users.noreply.github.com>
2024-02-03 23:55:04 -0600

    Create unit7_select_queries_with_outer_joins.sql

---
+++ unit7_select_queries_with_outer_joins.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT building FROM employees;
+
+SELECT * FROM buildings;
+
+SELECT DISTINCT role, building_name FROM buildings
+LEFT JOIN employees
+    ON buildings.building_name = employees.building;
